b2a9ecfac79ec807b6e1a3afe93f1e4183863f49
tolentinoel <rckrleyn@gmail.com>
2021-02-20 17:34:23 -0800

    heroku

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_12_02_004600) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "boards", force: :cascade do |t|
     t.string "theme"
     t.string "difficulty"
